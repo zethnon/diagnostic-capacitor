7a5b993f7d36b042bb4eab9cc775fc0a287e6e7e
zethnon <johnunes200@gmail.com>
2026-03-20 19:14:10 +0000

    packaging changes

--- diagnostic-capacitor/Package.swift
+++ diagnostic-capacitor/Package.swift
@@ -1,38 +1,30 @@
 // swift-tools-version: 5.9
 import PackageDescription
-
+// DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
 let package = Package(
-    name: "NoesisDiagnosticCapacitor",
+    name: "CapApp-SPM",
     platforms: [.iOS(.v15)],
     products: [
         .library(
-            name: "NoesisDiagnosticCapacitor",
-            targets: ["DiagnosticPlugin"])
+            name: "CapApp-SPM",
+            targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.1.0"),
+        .package(name: "CapacitorCamera", path: "../../../node_modules/@capacitor/camera"),
+        .package(name: "CapacitorSplashScreen", path: "../../../node_modules/@capacitor/splash-screen"),
+        .package(name: "NoesisDiagnosticCapacitor", path: "../../../../diagnostic-capacitor")
     ],
     targets: [
         .target(
-            name: "DiagnosticPluginObjC",
-            dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swift-pm")
-            ],
-            path: "ios/Sources/DiagnosticPluginObjC",
-            publicHeadersPath: "."
-        ),
-        .target(
-            name: "DiagnosticPlugin",
+            name: "CapApp-SPM",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
-                "DiagnosticPluginObjC"
-            ],
-            path: "ios/Sources/DiagnosticPlugin"
-        ),
-        .testTarget(
-            name: "DiagnosticPluginTests",
-            dependencies: ["DiagnosticPlugin"],
-            path: "ios/Tests/DiagnosticPluginTests")
+                .product(name: "CapacitorCamera", package: "CapacitorCamera"),
+                .product(name: "CapacitorSplashScreen", package: "CapacitorSplashScreen"),
+                .product(name: "NoesisDiagnosticCapacitor", package: "NoesisDiagnosticCapacitor")
+            ]
+        )
     ]
 )
